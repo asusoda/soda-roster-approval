0e065d3f908eeabe4396a0b4e5224a31029a3191
rtwoo <ryanw.contact@gmail.com>
2023-05-30 00:58:44 -0700

    Replace env var with password entry prompt

--- engage-roster-approver.py
+++ engage-roster-approver.py
@@ -11,12 +11,15 @@ from dotenv import load_dotenv
 import os
 import logging
 import sys
+from getpass import getpass
 
 logger = logging.getLogger()
 logger.setLevel(20)
 if len(sys.argv) > 1 and sys.argv[1] == '-v':
     print('Verbose mode enabled')
     verbose = True
+else:
+    verbose = False
 
 file_log_handler = logging.FileHandler('approved_members.log')
 stdout_log_handler = logging.StreamHandler(sys.stdout)
@@ -30,10 +33,10 @@ stdout_log_handler.setFormatter(formatter)
 
 load_dotenv()
 username = os.getenv('username')
-password = os.getenv('password')
 
 options = ChromeOptions()
 options.add_argument('--headless=new')
+options.add_argument(f'--user-data-dir={os.getcwd()}/userdata')
 driver = webdriver.Chrome(options=options)
 
 def accept_members(table):
@@ -45,7 +48,7 @@ def accept_members(table):
             name = member.find_element(By.XPATH, 'td[2]/span[1]').text
             member.find_element(By.XPATH, 'td[4]/button[1]').click()
             logger.info(f'Approved "{name}"')
-            time.sleep(5)
+            time.sleep(3)
         except:
             if verbose: logger.info('No requests, skipping...')
             break
@@ -53,19 +56,22 @@ def accept_members(table):
 
 def login():
     try:
+        driver.get('https://asu.campuslabs.com/engage/actioncenter/organization/soda/roster/Roster/prospective')
         WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="prospective"]/div/table/tbody')))
+        logger.info(f'Already logged in as {username}, skipping login process...')
         return True
     except:
         try:
             # Initial login process
-            driver.get('https://asu.campuslabs.com/engage/actioncenter/organization/soda/roster/Roster/prospective')
             WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'username'))).send_keys(username)
+            password = os.getenv('password')
+            if password is None:
+                password = getpass('Password: ')
             driver.find_element(By.ID, 'password').send_keys(password)
             driver.find_element(By.NAME, 'submit').click()
 
             # Duo 2FA
             WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@id="duo_iframe"]')))
-            # Assume making it this far means successful log in
             
             # Enable "Remember me for 7 days" checkbox
             WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login-form"]/div[2]/div/label/input'))).click()
@@ -79,16 +85,18 @@ def login():
 def main():
     if login():
         try:
-            table: WebElement = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="prospective"]/div/table/tbody')))
-            logger.info('Duo 2FA push accepted, proceeding to Engage')
+            table: WebElement = WebDriverWait(driver, 60).until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@id="prospective"]/div/table/tbody')))
+            logger.info('Authenticated, proceeding to Engage')
             while True:
                 accept_members(table)
                 time.sleep(60)
                 if verbose: logger.info(f'Refreshing...')
                 driver.refresh()
-                table = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="prospective"]/div/table/tbody')))
+                table = WebDriverWait(driver, 60).until(
+                    EC.visibility_of_element_located((By.XPATH, '//*[@id="prospective"]/div/table/tbody')))
         except Exception as e:
-            print(e)
+            logger.error(e)
             logger.error(f'Crashed...')
             main()
     else:
