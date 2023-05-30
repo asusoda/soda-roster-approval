5dd6b204fff40702943ca383a430745a6d54fa0e
rtwoo <ryanw.contact@gmail.com>
2023-05-29 21:22:44 -0700

    Modularized and more fault tolerance

--- engage-roster-approver.py
+++ engage-roster-approver.py
@@ -14,6 +14,9 @@ import sys
 
 logger = logging.getLogger()
 logger.setLevel(20)
+if len(sys.argv) > 1 and sys.argv[1] == '-v':
+    print('Verbose mode enabled')
+    verbose = True
 
 file_log_handler = logging.FileHandler('approved_members.log')
 stdout_log_handler = logging.StreamHandler(sys.stdout)
@@ -33,32 +36,64 @@ options = ChromeOptions()
 options.add_argument('--headless=new')
 driver = webdriver.Chrome(options=options)
 
-driver.get('https://asu.campuslabs.com/engage/actioncenter/organization/soda/roster/Roster/prospective')
-
-# Initial login process
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'username'))).send_keys(username)
-driver.find_element(By.ID, 'password').send_keys(password)
-driver.find_element(By.NAME, 'submit').click()
-
-# Duo 2FA
-WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@id="duo_iframe"]')))
-
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login-form"]/div[2]/div/label/input'))).click()
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="Send Me a Push"]'))).click()
-
-# Retrieve prospective member table
-while True:
-    try:
-        table: WebElement = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="prospective"]/div/table/tbody')))
-        members = table.find_elements(By.TAG_NAME, 'tr')
-        for member in members:
-            # Click 'APPROVE' button
+def accept_members(table):
+    # Retrieve prospective members
+    members = table.find_elements(By.TAG_NAME, 'tr')
+    for member in members:
+        # Click 'APPROVE' button
+        try:
             name = member.find_element(By.XPATH, 'td[2]/span[1]').text
             member.find_element(By.XPATH, 'td[4]/button[1]').click()
             logger.info(f'Approved "{name}"')
             time.sleep(5)
-        time.sleep(60)
-        driver.refresh()
+        except:
+            if verbose: logger.info('No requests, skipping...')
+            break
+
+
+def login():
+    try:
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="prospective"]/div/table/tbody')))
+        return True
     except:
-        driver.quit()
-        logger.error(f'Crashed...')
+        try:
+            # Initial login process
+            driver.get('https://asu.campuslabs.com/engage/actioncenter/organization/soda/roster/Roster/prospective')
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'username'))).send_keys(username)
+            driver.find_element(By.ID, 'password').send_keys(password)
+            driver.find_element(By.NAME, 'submit').click()
+
+            # Duo 2FA
+            WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@id="duo_iframe"]')))
+            # Assume making it this far means successful log in
+            
+            # Enable "Remember me for 7 days" checkbox
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login-form"]/div[2]/div/label/input'))).click()
+            
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="Send Me a Push"]'))).click()
+            logger.info(f'Logged in as {username}, sending Duo 2FA push...')
+            return True
+        except:
+            return False
+
+def main():
+    if login():
+        try:
+            table: WebElement = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="prospective"]/div/table/tbody')))
+            logger.info('Duo 2FA push accepted, proceeding to Engage')
+            while True:
+                accept_members(table)
+                time.sleep(60)
+                if verbose: logger.info(f'Refreshing...')
+                driver.refresh()
+                table = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="prospective"]/div/table/tbody')))
+        except Exception as e:
+            print(e)
+            logger.error(f'Crashed...')
+            main()
+    else:
+        main()
+
+if __name__ == "__main__":
+    main()
+    # driver.quit()
