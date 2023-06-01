c9a2e39c5925dfef7e8ffa8ecd353cbd51626230
rtwoo <ryanw.contact@gmail.com>
2023-05-31 20:56:12 -0700

    Minor changes

--- engage-roster-approver.py
+++ engage-roster-approver.py
@@ -38,8 +38,6 @@ stdout_log_handler.setFormatter(formatter)
 options = ChromeOptions()
 options.arguments.extend([
     '--headless=new',
-    '--no-sandbox',
-    '--disable-dev-shm-usage',
     f'--user-data-dir={os.getcwd()}/userdata'
 ])
 driver = webdriver.Chrome(options=options)
@@ -76,17 +74,21 @@ def login():
         try:
             try:
                 # Initial login process
-                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'username'))).send_keys(username)
+                WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.ID, 'username'))).send_keys(username)
                 driver.find_element(By.ID, 'password').send_keys(password)
                 driver.find_element(By.NAME, 'submit').click()
             finally:
                 # Duo 2FA
-                WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@id="duo_iframe"]')))
+                WebDriverWait(driver, 20).until(
+                    EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@id="duo_iframe"]')))
                 
                 # Enable "Remember me for 7 days" checkbox
-                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login-form"]/div[2]/div/label/input'))).click()
+                WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.XPATH, '//*[@id="login-form"]/div[2]/div/label/input'))).click()
                 
-                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="Send Me a Push"]'))).click()
+                WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="Send Me a Push"]'))).click()
                 logger.info(f'Logged in as {username}, sending Duo 2FA push...')
                 return True
         except:
